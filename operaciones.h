d0ae3d606caf36bf0d144f2f09c8f2fb0ac9e5a1
SergioCaulfield <aguirres837@gmail.com>
2025-09-09 00:02:30 -0600

    Funciones de operaciones y incrementos, decrementos

--- operaciones.h
+++ operaciones.h
@@ -1,12 +1,13 @@
-#include "operaciones.h"
-#include <stdio.h>
 #ifndef OPERACIONES_H
 #define OPERACIONES_H
+#include <stdio.h>
 
 int sumar(int a, int b);
 int restar(int a, int b);
 int multiplicar(int a, int b);
 int dividir(int a, int b);
+int incrementar(int a);
+int decrementar(int a);
 
 int sumar(int a, int b) {
     return a + b;
@@ -26,5 +27,13 @@ int dividir(int a, int b) {
     }
     return a / b;
 }
+
+int incrementar(int a) {
+    return a + 1;
+}
+int decrementar(int a) {
+    return a - 1;
+}
+
 
 #endif
