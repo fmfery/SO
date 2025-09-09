3fb414303a91edebf2b1b63f5fe36c0e70afbd74
SergioCaulfield <aguirres837@gmail.com>
2025-09-08 23:20:35 -0600

    Update operaciones.h

--- operaciones.h
+++ operaciones.h
@@ -1,3 +1,5 @@
+#include "operaciones.h"
+#include <stdio.h>
 #ifndef OPERACIONES_H
 #define OPERACIONES_H
 
@@ -5,5 +7,24 @@ int sumar(int a, int b);
 int restar(int a, int b);
 int multiplicar(int a, int b);
 int dividir(int a, int b);
+
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int restar(int a, int b) {
+    return a - b;
+}
+
+int multiplicar(int a, int b) {
+    return a * b;
+}
+
+int dividir(int a, int b) {
+    if (b == 0) {
+        return 0;
+    }
+    return a / b;
+}
 
 #endif
